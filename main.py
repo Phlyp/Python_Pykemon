f84c1bcb0b14bbcb2cadd83fa6259595a66cbc36
Phlyp <pypruessner@gmail.com>
2022-06-05 17:04:53 +0200

    added docstrings to main and system_calls

--- main.py
+++ main.py
@@ -12,13 +12,20 @@ import pygame
 
 clear = lambda: os.system('cls')
 
-"""
-Main menu game loop
 
-*inputs: none
-*returns: none
-"""
 def game_engine():
+    """
+    game_engine Main menu game loop
+
+    This Loop functions as the main menu for the app, allowing the player to choose which sub-functionality to execute next
+
+    Args: None
+    Returns: None
+
+    Test:
+        * test inputs 1-4 and their expected outcomes
+        * other inputs should not be accepted
+    """                
     while True:
         sys.clear()
         print(f"Hello {player.current_player.name}, what would you like to do?")
@@ -43,13 +50,18 @@ def game_engine():
         else:
             print("invalid input given!")
 
-"""
-player settings main menu
 
-*inputs: none
-*returns: none
-"""
 def player_settings():
+    """
+    player_settings Main menu to manage the players
+
+    Args: None
+    Returns: None
+
+    Test:
+        * test inputs 1-5 and their expected outcomes
+        * other inputs should not be accepted 
+    """    
     while True:
         sys.clear()
         print(f"Hello {player.current_player.name}, what would you like to do?")
@@ -68,7 +80,6 @@ def player_settings():
             player.delete_all_players()
             team.delete_all_teams()
         elif decsision == "5":
-            print("")
             break
         else:
             print("invalid input given!")
@@ -80,6 +91,16 @@ team settings main menu
 *outputs: none
 """
 def team_settings():
+    """
+    team_settings Main menu to manage the players team of pokemon
+
+    Args: None
+    Returns: None
+
+    Test:
+        * test inputs 1-4s and their expected outcomes
+        * other inputs should not be accepted 
+    """    
     while True:
         sys.clear()
         print(f"Hello {player.current_player.name}, what would you like to do?")
@@ -110,21 +131,17 @@ def team_settings():
         print("\n") 
 
 
-"""
-main program funcion
-checks if database exists, 
-    if not a new player must be created first
-    else prompts to select a player
-starts main menu loop
-
-"""
 if __name__ == "__main__":
+    # main program funcion initiatlises Database
     is_new_session = not database.table_exists("team")
     database.initialise()
     sys.clear()
     print("Welcome to Pykemon!\n")
     sys.wait_for_keypress()
 
+    # checks if database exists, 
+    #     if not a new player must be created first
+    #     else gives the user the option to choose (select existing player) or (create new player)
     if is_new_session:
         sys.clear()
         print("As you are starting Pykemon for the first time, you must first create a player!")
@@ -134,6 +151,7 @@ if __name__ == "__main__":
         print("Would you like to:")
         print(" 1. Select an exisiting Player\n 2. Create a new player")
         
+        # loop accepts inputs 1-2 and prompts user for new input otherwise
         while True:
             decision = input("Please use the Keys 1-2 + ENTER to choose what to do next! ")
 
@@ -150,5 +168,5 @@ if __name__ == "__main__":
                 print("Invalid Input given!")
                 continue
             
-
+    # starts the main menu game loop
     game_engine()
